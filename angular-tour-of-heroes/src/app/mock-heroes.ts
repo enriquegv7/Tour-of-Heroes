bb21e8cc2f0bd12a34c1984c720efe47c16407eb
enriquegv7 <enrique4597@gmail.com>
2022-10-24 17:33:32 +0200

    2. Display a list

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 2, name: 'Juno' },
+    { id: 3, name: 'Neptuno' },
+    { id: 4, name: 'Apolo' },
+    { id: 5, name: 'Minerva' },
+    { id: 6, name: 'Venus' },
+    { id: 7, name: 'Marte' },
+    { id: 8, name: 'Baco' },
+    { id: 9, name: 'Mercurio' },
+    { id: 10, name: 'Diana' }
+]
